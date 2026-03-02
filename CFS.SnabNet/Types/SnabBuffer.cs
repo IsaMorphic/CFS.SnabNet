5b9522b575af9726440119fb027f20b162e7468c
Isabelle Santin <isabelle@chosenfewsoftware.com>
2026-03-02 16:12:31 -0600

    Fix: `SnabBuffer` endianness bug

--- CFS.SnabNet/Types/SnabBuffer.cs
+++ CFS.SnabNet/Types/SnabBuffer.cs
@@ -4,17 +4,19 @@ namespace CFS.SnabNet.Types
     internal class SnabBuffer : ISnabType<byte[]>
     {
         public HashSet<byte> TypeIds { get; }
+
         public SnabBuffer()
         {
             TypeIds = new HashSet<byte> { 0x0B };
         }
+
         public byte[] ReadFromInstance(SnabReader instance, byte typeId)
         {
             if (typeId != 0x0B)
                 throw new InvalidDataException($"Invalid typeId {typeId} for SnabBuffer");
 
             Span<byte> bytesSpan = stackalloc byte[sizeof(uint)];
-            if(BitConverter.IsLittleEndian ^ 
+            if(BitConverter.IsLittleEndian ==
                 instance.Header.Flags.HasFlag(SnabFlags.BigEndian)) 
             {
                 bytesSpan.Reverse();
